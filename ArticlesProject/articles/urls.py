f7c5bf104a53e83bb1b3002f4f3761eb0df7ec73
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-16 16:23:50 +0300

    add article detail view

--- ArticlesProject/articles/urls.py
+++ ArticlesProject/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleCreateView, ArticlesListView
+from .views import ArticleCreateView, ArticlesListView, ArticleDetailView
 
 
 urlpatterns = [
     path('article/create/', ArticleCreateView.as_view()),
     path('article/', ArticlesListView.as_view()),
+    path('article/<int:pk>/', ArticleDetailView.as_view())
 ]
--- ArticlesProject/articles/views.py
+++ ArticlesProject/articles/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .serializers import ArticleDetailSerializer, ArticlesListSerializer
 from .models import Article
 
 
 class ArticleCreateView(generics.CreateAPIView):
-    """View for article creation"""
+    """View for article creating"""
     serializer_class = ArticleDetailSerializer
 
 
 class ArticlesListView(generics.ListAPIView):
     """View for article list getting"""
     serializer_class = ArticlesListSerializer
+    queryset = Article.objects.all()
+
+
+class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View for article getting, updating, deleting"""
+    serializer_class = ArticleDetailSerializer
     queryset = Article.objects.all()
