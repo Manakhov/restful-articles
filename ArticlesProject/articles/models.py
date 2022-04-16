5ab63ed16d496c79ef32080381e22293a02dc52b
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-16 11:59:33 +0300

    add Article model and Create view

--- ArticlesProject/articles/models.py
+++ ArticlesProject/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name='Заголовок', unique=True, max_length=64)
+    body = models.TextField(verbose_name='Тело')
+    article_types = (
+        (1, 'Публичная'),
+        (2, 'Закрытая'),
+    )
+    type = models.IntegerField(verbose_name='Тип', choices=article_types)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
