20d2706ab9793a6c26532ee7e85cbd16265050e5
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-18 12:57:04 +0300

    add comment for permission

--- ArticlesProject/articles/permissions.py
+++ ArticlesProject/articles/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Returns True if the method is safe or the user is the article author"""
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
