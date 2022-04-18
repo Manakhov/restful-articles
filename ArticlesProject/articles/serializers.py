4d0ba5305cc3bf1b7254031cd09f7d403dcde3de
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-18 16:16:41 +0300

    change author_id -> author_name

--- ArticlesProject/articles/serializers.py
+++ ArticlesProject/articles/serializers.py
@@ -4,6 +4,8 @@ from .models import Article
 
 class ArticlesListSerializer(serializers.ModelSerializer):
     """Serializer for articles list"""
+    author = serializers.CharField()
+
     class Meta:
         model = Article
         fields = ('id', 'title', 'author')
@@ -12,6 +14,7 @@ class ArticlesListSerializer(serializers.ModelSerializer):
 class ArticleDetailSerializer(serializers.ModelSerializer):
     """Serializer for one article"""
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    author_name = serializers.CharField(read_only=True, source='author')
 
     class Meta:
         model = Article
