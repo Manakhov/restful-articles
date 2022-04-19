d309b58c83ca610ea9af000e37642671130164fb
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-19 14:36:53 +0300

    add superuser for Django admin

--- ArticlesProject/users/models.py
+++ ArticlesProject/users/models.py
@@ -1,24 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class MyUserManager(BaseUserManager):
     """Custom user manager model"""
 
-    def create_user(self, email, role, password=None):
-        user = self.model(email=email, role=role)
+    def create_user(self, email, role, password=None, **extra_fields):
+        user = self.model(email=email, role=role, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, role, password=None):
-        user = self.create_user(email=email, role=role, password=password)
-        user.is_stuff = True
-        user.save()
+    def create_superuser(self, email, role, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(email=email, role=role, password=password, **extra_fields)
         return user
 
 
-class MyUser(AbstractBaseUser):
+class MyUser(AbstractBaseUser, PermissionsMixin):
     """Custom user model"""
     email = models.EmailField(verbose_name='Адрес электронной почты', unique=True, max_length=64)
     user_roles = (
@@ -28,6 +28,7 @@ class MyUser(AbstractBaseUser):
     role = models.IntegerField(verbose_name='Роль', choices=user_roles)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['role']
