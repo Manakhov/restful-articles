b437ac6345c12dc3e99fc9ca2fac78f3bd466875
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-19 22:36:24 +0300

    add password validate

--- ArticlesProject/users/serializers.py
+++ ArticlesProject/users/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -6,6 +7,14 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for a user"""
     password = serializers.CharField(write_only=True)
+
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise serializers.ValidationError("Пароль должен быть не короче 8 символов.")
+        elif not bool(re.search(r'\d', password)):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну цифру.")
+        elif not bool(re.search(r'[a-zA-Z]', password)):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну букву любого регистра.")
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
