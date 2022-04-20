2e656512d7bede2df29f4cc82de89b8c893fc8ce
Manakhov <manakhovsvyatoslav@gmail.com>
2022-04-20 09:54:31 +0300

    delete session authentication

--- ArticlesProject/ArticlesProject/urls.py
+++ ArticlesProject/ArticlesProject/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
     path('article/', include('articles.urls')),
     path('user/', include('users.urls')),
 ]
