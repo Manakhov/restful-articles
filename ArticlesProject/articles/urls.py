d99c718f69d66a03de4b245d911bd81abaa49202
Manakhov <manakhovsvyatoslav@gmail.com>
2022-05-01 17:22:13 +0300

    use ViewSet

--- ArticlesProject/articles/urls.py
+++ ArticlesProject/articles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ArticleListCreateView, ArticleDetailView
+from .views import ArticleViewSet
 
 
 urlpatterns = [
-    path('', ArticleListCreateView.as_view()),
-    path('<int:pk>/', ArticleDetailView.as_view()),
+    # path('', ArticleListCreateView.as_view()),
+    # path('<int:pk>/', ArticleDetailView.as_view()),
+    path('', ArticleViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('<int:pk>/', ArticleViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
--- ArticlesProject/articles/views.py
+++ ArticlesProject/articles/views.py
@@ -1,23 +1,49 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from .models import Article
 from .permissions import IsOwnerOrReadOnly, IsAuthorOrReadOnly, IsPublicArticleAndAnonymUser
 
 
-class ArticleListCreateView(generics.ListCreateAPIView):
-    """View for article list getting or article creating"""
-    serializer_class = ArticleSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+# class ArticleListCreateView(generics.ListCreateAPIView):
+#     """View for article list getting or article creating"""
+#     serializer_class = ArticleSerializer
+#     permission_classes = (IsAuthorOrReadOnly,)
+#
+#     def get_queryset(self):
+#         user = self.request.user
+#         if user.id is None:
+#             return Article.objects.filter(type=1)
+#         return Article.objects.all()
+#
+#
+# class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """View for article getting, updating, deleting"""
+#     serializer_class = ArticleSerializer
+#     queryset = Article.objects.all()
+#     permission_classes = (IsOwnerOrReadOnly, IsPublicArticleAndAnonymUser,)
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.id is None:
-            return Article.objects.filter(type=1)
-        return Article.objects.all()
 
+class ArticleViewSet(viewsets.ViewSet):
+    """ViewSet for article"""
 
-class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """View for article getting, updating, deleting"""
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-    permission_classes = (IsOwnerOrReadOnly, IsPublicArticleAndAnonymUser,)
+    def list(self, request):
+        queryset = Article.objects.all()
+        serializer = ArticleSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
