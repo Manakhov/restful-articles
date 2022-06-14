4e58314bb753ad17aae4a0d26448f8e114596ef3
Manakhov <manakhovsvyatoslav@gmail.com>
2022-06-14 17:59:30 +0300

    return Session Auth for tests

--- ArticlesProject/articles/urls.py
+++ ArticlesProject/articles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ArticleViewSet
+from django.urls import include  # for tests
 
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     # path('<int:pk>/', ArticleDetailView.as_view()),
     path('', ArticleViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('<int:pk>/', ArticleViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('auth/', include('rest_framework.urls')),  # for tests
 ]
